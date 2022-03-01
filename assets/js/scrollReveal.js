9114ed39477f58a2742b33cdd7b9095a7a1400f5
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-02-28 23:27:37 -0300

    add lib scrollReveal

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal('.text',{
+    origin: 'left',
+    distance: '100px',
+    duration:600,
+    delay: 1000
+});
+
+ScrollReveal().reveal('.img',{
+    origin: 'right',
+    distance: '100px',
+    duration:600,
+    delay: 1500
+});
+
+ScrollReveal().reveal('.card',{
+    origin: 'bottom',
+    distance: '100px',
+    duration:800,
+    delay: 1400
+});
