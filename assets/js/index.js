fa47bcac0c3510985361eca2a95ce4613a4d3300
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-03-01 22:51:31 -0300

    add form contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,95 @@ function activeLink(){
 }
 list.forEach((item) =>
 item.addEventListener('click', activeLink))
+
+/* FORM REQUIRED VALIDATION CUSTOM */
+const fields = document.querySelectorAll("[required]")
+
+function ValidateField(field) {
+    // logica para verificar se existem erros
+    function verifyErrors() {
+        let foundError = false;
+
+        for(let error in field.validity) {
+            // se não for customError
+            // então verifica se tem erro
+            if (field.validity[error] && !field.validity.valid ) {
+                foundError = error
+            }
+        }
+        return foundError;
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                valueMissing: "E-mail é obrigatório",
+                typeMismatch: "Por favor, preencha um email válido"
+            },
+            tel: {
+                valueMissing: "N° celular é obrigatório"
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+        
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            field.style.borderColor = "#d43a3a"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "green"
+            setCustomMessage()
+        }
+    }
+}
+
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = ValidateField(field)
+
+    validation()
+
+}
+
+for( field of fields ){
+    field.addEventListener("invalid", event => { 
+        // eliminar o bubble
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
+}
+
+
+document.querySelector("form")
+.addEventListener("submit", event => {
+    console.log("enviar o formulário")
+
+    // não vai enviar o formulário
+    event.preventDefault()
+})
